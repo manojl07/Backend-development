6c25376d97234fbaff89f17fdd7b9855c67d0f99
manoj <manoj.l2k01@gmail.com>
2026-04-14 14:53:32 +0530

    Using jwt token and signing it and giving user

--- day-10/src/routes/auth.routes.js
+++ day-10/src/routes/auth.routes.js
@@ -10,7 +10,7 @@ authRouter.post("/register", async (req, res) => {
   const isUserAlreadyExists = await userModel.findOne({ email })
 
   if (isUserAlreadyExists) {
-    return res.status(400).json({ message: "User already EXISTS!" })
+    return res.status(409).json({ message: "User already EXISTS!" })
   }
 
   const user = await userModel.create({
@@ -34,6 +34,38 @@ authRouter.post("/register", async (req, res) => {
   })
 
 })
+
+// controller
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email })
+  if(!user){
+    return res.status(404).json({
+      message: "User not found with this email address"
+    })
+  }
+
+  const isPasswordMatched = await user.password === password;
+  if(!isPasswordMatched){
+    return res.status(401).json({
+      message: "Invalid password"
+    })
+  }
+
+  const token = jwt.sign({
+    id: user._id
+  }, process.env.JWT_SECRET)
+
+  res.cookie("jwt_token", token)
+
+  res.status(200).json({
+    message: "User Logged In...!",
+    user
+  })
+})
+
+
 
 
 module.exports = authRouter;
